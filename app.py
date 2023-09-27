e727c2238f8a843af8e6f523ec90c977a29427e8
Durrani123 <durraniabdurehman@gmail.com>
2023-09-27 17:49:36 +0500

    Updated

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify, request,send_file
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
-import io
 import os
 import json
 import matplotlib.pyplot as plt
@@ -8,36 +7,21 @@ import pandas as pd
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # Allow requests from http://localhost:3000
+# Import other modules from your project. For example:
+# from visualization.generate_chart import generate_chart
 
-
-from visualization.generate_chart import generate_chart
-
-
-# generates image
-@app.route("/api/home", methods=['GET', 'POST'])  
+@app.route("/api/home", methods=['GET', 'POST'])
 def return_home():
     if request.method == 'POST':
         file = request.files["file"]
-
-        columns_json = request.form.get("columns")
-        chosenReq_json = request.form.get("chosenReq")
-        chart_json = request.form.get("chart")
-        selectedCustom_json = request.form.get("selectedCustom")
-
-        columns = json.loads(columns_json)
-        chosenReq = json.loads(chosenReq_json)
-        chart = json.loads(chart_json)
-        selectedCustom = json.loads(selectedCustom_json)
-        
-        fig = generate_chart(file,columns,chosenReq,chart,selectedCustom)
-        chart_data = fig.to_json()
+        # Rest of your route code here...
         return jsonify({'chartData': chart_data})
 
     return jsonify({
-        'message': "Hellos World"
+        'message': "Hello World"
     })
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(host="0.0.0.0", port=8000)
